df0a564212e3311c21722bfa330ae1c7645c799b
HShan99 <shanharsharathnayake@gmail.com>
2025-01-04 13:34:10 +0530

    Create the navigation bar

--- index.php
+++ index.php
@@ -1,14 +1,59 @@
 <!DOCTYPE html>
 <html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Student Management System</title>
-</head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Student Management System</title>
+<link rel="stylesheet" type="text/css" href="style.css">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 
 <body>
-    <h1>this is the main page</h1>
+    <nav>
+        <label class="logo">W-School</label>
+        <ul>
+            <li><a href="">Home</a></li>
+            <li><a href="">Contact</a></li>
+            <li><a href="">Admission</a></li>
+            <li><a href="" class="btn btn-primary">Login</a></li>
+        </ul>
+    </nav>
+
+    <div class="section1">
+        <label class="img_text">The Student Management</label>
+        <img class="main_image" src="images/backgound.jpg" alt="image">
+    </div>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4">
+                <img class="ground_image" src="images/ground.jpg" alt="image">
+            </div>
+            <div class="col-md-8">
+                <h1>Welcome to W-School</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Mollitia, molestias deleniti voluptatem
+                    molestiae suscipit vitae similique officia debitis. Consequuntur eligendi, esse nulla dolor fuga
+                    impedit unde veritatis magni in quam!Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    Mollitia, molestias deleniti voluptatem
+                    molestiae suscipit vitae similique officia debitis. Consequuntur eligendi, esse nulla dolor fuga
+                    impedit unde veritatis magni in quam!Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    Mollitia, molestias deleniti voluptatem
+                    molestiae suscipit vitae similique officia debitis. Consequuntur eligendi, esse nulla dolor fuga
+                    impedit unde veritatis magni in quam!</p>
+            </div>
+        </div>
+    </div>
+
+
+
+
+
+
+
+
 </body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
+</script>
 
 </html>
