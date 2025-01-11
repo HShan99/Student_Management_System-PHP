174106a4acaf5a7282f4227416755942293804c1
HShan99 <shanharsharathnayake@gmail.com>
2025-01-11 18:52:52 +0530

    Create the admission part

---
+++ admin_sidebar.php
@@ -0,0 +1,18 @@
+<header class="header">
+    <a href="" class="links">Admin Dashboard</a>
+
+    <div class="logout">
+        <a href="logout.php" class="btn btn-primary">Logout</a>
+    </div>
+</header>
+<aside>
+    <ul>
+        <li><a href="admission.php" class="links">Admission</a></li>
+        <li><a href="" class="links">Add Student</a></li>
+        <li><a href="" class="links">View Student</a></li>
+        <li><a href="" class="links">Add Teacher</a></li>
+        <li><a href="" class="links">View Teacher</a></li>
+        <li><a href="" class="links">Add Course</a></li>
+        <li><a href="" class="links">View Corse</a></li>
+    </ul>
+</aside>
