509c5ff5e20b6ea41a7e493e490c6bc5d7df49c4
HShan99 <shanharsharathnayake@gmail.com>
2025-01-12 22:53:41 +0530

    Create view students part

---
+++ viewStudent.php
@@ -0,0 +1,82 @@
+<?php
+    session_start();
+    
+    if(!isset($_SESSION['userName'])){
+        header("location:login.php");
+    }
+    elseif($_SESSION['user_type']=='student'){
+        header("location:login.php");
+    }
+    
+$host="localhost";
+$user = "root";
+$password = "";
+$db = "w-school";
+
+$data=mysqli_connect($host, $user, $password, $db);
+$sql= "SELECT * FROM user WHERE user_type='student'";
+
+$result = mysqli_query($data,$sql);
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin Dashboard</title>
+    <?php include 'admin_css.php'; ?>
+</head>
+
+<body>
+    <?php
+        include 'admin_sidebar.php';
+    ?>
+    <div class="content">
+        <center style="font-size:30px; font-weight:bold; padding-bottom:50px;">Registered Students</center>
+
+        <center>
+            <table class="table" id="tableStyle">
+                <thead>
+                    <tr>
+                        <th scope="col">Name</th>
+                        <th scope="col">Email</th>
+                        <th scope="col">Phone</th>
+
+                    </tr>
+                </thead>
+                <tbody>
+
+                    <?php 
+                    while($info=$result->fetch_assoc()){
+                ?>
+                    <tr>
+                        <th scope="row">
+                            <?php echo "{$info['user_name']}"?>
+                        </th>
+                        <td><?php echo "{$info['email']}"?></td>
+                        <td><?php echo "{$info['phone']}"?></td>
+
+                    </tr>
+                    <?php
+                }
+                ?>
+                </tbody>
+            </table>
+        </center>
+
+    </div>
+
+
+
+
+
+
+
+
+
+</body>
+
+</html>
